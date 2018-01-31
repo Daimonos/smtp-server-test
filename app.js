057d504550e334fa227ad3fea43310f9b1140e77
Daimonos <jim.d.hughes@gmail.com>
2018-01-31 13:04:18 -0700

    fixing a bug

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res, next){
-
+  console.log('Returning List');
   db.getDocuments(function(err, docs){
     let mail = _.map(docs.rows, d=>{d.doc.mail._id = d.doc._id; return d.doc.mail});
     res.render('index', {mail:mail});
-  })
+  });
 });
 
 app.get('/:id', function(req, res, next){
+  console.log('Returning a single mail item');
   db.getDocument(req.params.id, function(err, doc){
-    res.render('mail', {mail:doc.mail})
-  })
-})
+    let mail = doc?doc.mail:null;
+    res.render('mail', {mail:mail})
+  });
+});
 module.exports = app;
